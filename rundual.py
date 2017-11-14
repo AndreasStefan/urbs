74d182b9fb31ddc9eec311c7dc600d93aaaa132d
AnderlStef <andreas.stefan90@gmail.com>
2017-11-14 23:44:08 +0100

    update

--- rundual.py
+++ rundual.py
@@ -1,16 +1,18 @@
 import pyomo.environ
 import urbs
+import pandas as pd
 from pyomo.core import Constraint
 from pyomo.opt.base import SolverFactory
 import runme
 data = urbs.read_excel('Augsburg.xlsx')
 
-data = urbs.runme.scenario_base(data)
-prob = urbs.create_model(data, timesteps=range(0, 100), dual=True)
+data = runme.scenario_base(data)
+prob = urbs.create_model(data, timesteps=range(0, 5), dual=True)
 
 optim = SolverFactory('gurobi')
 result = optim.solve(prob, tee=True)
 
 res_vertex_duals = urbs.get_entity(prob, 'res_vertex')
-marg_costs = res_vertex_duals.xs(('Elec', 'Demand'), level=('com', 'com_type'))
+marg_costs = pd.DataFrame(res_vertex_duals.xs(('Elec', 'Demand'), level=('com', 'com_type'))).sort_index(axis=0,level='t')
+
 print(marg_costs)
